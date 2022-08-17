d6748bfbd57bb62950f2a301b32e2bbe3d4486b1
OsvaldoDominguez <111076178+OsvaldoDominguez@users.noreply.github.com>
2022-08-16 19:07:05 -0500

    create OsvaldoPiliado.txt

---
+++ conteo-inverso.py
@@ -0,0 +1,8 @@
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16, 17, 18, 19, 20, 21, 22, 23, 24]
+def invertir_listas(lista):
+    if linsta ==[ ]:
+        return lista
+    else:
+        return inversa = [lista [-1]] + invertir_lista(lista[:-1])
+        
+print(invertir_listas(lista))
