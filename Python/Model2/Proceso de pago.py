4266470e58f3f57766518a63fb127878099bba23
kikemx <kike@gmail.com>
2022-08-27 18:12:18 -0500

    JavaAndPython

---
+++ Python/Model2/Proceso de pago.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Proceso de pago:
+    def __init__(self):
+        self.Realizacion de cobro = None
+        self.Factura de pago = None
+
+    def Pago realizado(self, ):
+        pass
+
+    def Notificacion de pago recibido(self, ):
+        pass
+
---
+++ java/Model/Dar de alta.java
@@ -0,0 +1,52 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Dar de alta {
+
+    /**
+     * Default constructor
+     */
+    public Dar de alta() {
+    }
+
+    /**
+     * 
+     */
+    private float Llenar cantidad de stock;
+
+    /**
+     * 
+     */
+    private String Ingresar precio de venta;
+
+    /**
+     * 
+     */
+    private String Ingresar caracteristicas de producto;
+
+
+    /**
+     * 
+     */
+    private void Verificar existencia de stock() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    private void Ingresar precio a venta() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    private void Ingresa caracteristicas principales del producto() {
+        // TODO implement here
+    }
+
+}
